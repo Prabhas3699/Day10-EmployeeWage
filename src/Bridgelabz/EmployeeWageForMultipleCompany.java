314c886ca7ec95fb2d9d672bfca3a1be67d80fe1
Prabhas3699 <prabhak620@gmail.com>
2022-08-28 13:02:49 +0530

    Refactor Employee wage for multiple company

---
+++ src/Bridgelabz/EmployeeWageForMultipleCompany.java
@@ -0,0 +1,12 @@
+package Bridgelabz;
+
+public class EmployeeWageForMultipleCompany {
+	
+	public static void main(String []args) {
+		/*
+		 * calling method for RefactorEmployeeWage
+		 */
+		RefactorEmployeeWageUC8 wage= new RefactorEmployeeWageUC8();
+		wage.RefactorEmployeeWage("Google", 25, 100, 20);		
+	}
+}
