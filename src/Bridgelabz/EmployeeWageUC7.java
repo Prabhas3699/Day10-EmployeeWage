822f81c9eb0df0821d5d04dd7adc5865d93f888e
Prabhas3699 <prabhak620@gmail.com>
2022-08-28 12:59:56 +0530

    Refactor Employee wage

---
+++ src/Bridgelabz/EmployeeWageUC7.java
@@ -0,0 +1,12 @@
+package Bridgelabz;
+
+public class EmployeeWageUC7 {
+	
+	public static void main(String[] args) {
+		/*
+		 * calling method for RefactorEmployeeWage
+		 */
+		RefactorEmployeeWageUC7 wage=new RefactorEmployeeWageUC7();		
+		wage.RefactorEmployeeWage();
+	}
+}
